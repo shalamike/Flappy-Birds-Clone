d1a645016b32557050db1399c6da080990aa2b1a
shalamike <mshala02@dcs.bbk.ac.uk>
2020-03-02 21:02:09 +0000

    handling user input so bird flaps

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.MyGdxGame;
@@ -16,6 +17,8 @@ public class PlayState extends State {
 
     @Override
     public void handleInput() {
+        if(Gdx.input.justTouched())
+            bird.jump();
     }
 
     @Override
